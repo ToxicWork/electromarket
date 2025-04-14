6943c3dbcbe589e677c444d5564815dd7427c3ad
ToxicWork <sergiowgster@gmail.com>
2025-04-14 20:24:50 -0300

    .

---
+++ src/main/java/com/uade/tpo/electromarket/ElectromarketApplication.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.electromarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectromarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectromarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/tpo/electromarket/ElectromarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.electromarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectromarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
