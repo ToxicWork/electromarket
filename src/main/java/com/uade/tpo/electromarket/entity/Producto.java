dde8a49d68de3c45c23a0f87a6c211670369e0f0
ToxicWork <sergiowgster@gmail.com>
2025-04-16 23:51:06 -0300

    + Producto Entity

--- src/main/java/com/uade/tpo/electromarket/entity/Producto.java
+++ src/main/java/com/uade/tpo/electromarket/entity/Producto.java
@@ -1,10 +1,36 @@
 package com.uade.tpo.electromarket.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class Producto {
+
+    public Producto(String nombre, String descripcion){
+        this.nombre         = nombre;
+        this.descripcion    = descripcion;
+        this.stock          = 0;
+        this.precio         = 0;
+    }
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
     
+    @Column
+    private String nombre;
+
+    @Column
+    private String descripcion;
+
+    @Column
+    private long stock;
+
+    @Column
+    private float precio;
 }
