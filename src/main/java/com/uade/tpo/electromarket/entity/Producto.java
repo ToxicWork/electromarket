24b4e39f5f71ddd3e6f99dcd603eca2e87c3a27e
ToxicWork <sergiowgster@gmail.com>
2025-04-17 00:19:11 -0300

    + relacion de entidad en la DB

--- src/main/java/com/uade/tpo/electromarket/entity/Producto.java
+++ src/main/java/com/uade/tpo/electromarket/entity/Producto.java
@@ -1,10 +1,14 @@
 package com.uade.tpo.electromarket.entity;
 
+import java.util.Locale.Category;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
@@ -33,4 +37,8 @@ public class Producto {
 
     @Column
     private float precio;
+
+    @OneToOne
+    @JoinColumn(name = "categoria_id", referencedColumnName = "id")
+    private Categoria categoria;
 }
